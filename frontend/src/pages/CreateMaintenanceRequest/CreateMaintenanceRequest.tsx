183d16da0efc1d38160f221b69d0cbe6165a0060
poonchub <141898614+poonchub@users.noreply.github.com>
2025-02-28 22:07:48 +0700

    update 28/02/68

--- frontend/src/pages/CreateMaintenanceRequest/CreateMaintenanceRequest.tsx
+++ frontend/src/pages/CreateMaintenanceRequest/CreateMaintenanceRequest.tsx
@@ -1,11 +1,91 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./CreateMaintenanceRequest.css"
 
 import { Steps } from "antd";
 
 function CreateMaintenanceRequest() {
+    const [roomType, setRoomType] = useState()
 
-    const [selectedValue, setSelectedValue] = useState("1");
+    const [selectedArea, setSelectedArea] = useState(1);
+    const [selectedRoomType, setSelectedRoomType] = useState(1)
+    const [selectedFloor, setSelectedFloor] = useState(1)
+    const [selectedRoom, setSelectedRoom] = useState(1)
+    const [areaDescription, setAreaDescription] = useState("")
+    const [taskDescription, setTaskDescription] = useState("")
+    const [name, setName] = useState("พูลทรัพย์ นานาวัน");
+    const [phone, setPhone] = useState("0985944576");
+    const [email, setEmail] = useState("poonchubnanawan310@gmail.com");
+
+    const [onEdit, setOnEdit] = useState(false);
+
+    const [files, setFiles] = useState<File[]>([]);
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    const isValidImage = (file: File) => {
+        return file.type.startsWith("image/");
+    };
+
+    const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        let droppedFiles = Array.from(event.dataTransfer.files).filter(isValidImage);
+
+        if (droppedFiles.length > 3) {
+            droppedFiles = droppedFiles.slice(0, 3);
+            alert("สามารถเลือกได้สูงสุด 3 ไฟล์");
+        }
+
+        setFiles(droppedFiles);
+    };
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            let selectedFiles = Array.from(event.target.files).filter(isValidImage);
+
+            if (selectedFiles.length > 3) {
+                selectedFiles = selectedFiles.slice(0, 3);
+                alert("สามารถเลือกได้สูงสุด 3 ไฟล์");
+            }
+
+            setFiles(selectedFiles);
+        }
+    };
+
+    const area = [
+        {
+            ID: 1,
+            AreaName: "ห้องทำงาน/ห้องประชุม"
+        },
+        {
+            ID: 2,
+            AreaName: "ภายในและบริเวณรอบอาคาร"
+        }
+    ]
+
+    const roomtype = [
+        {
+            ID: 1,
+            TypeName: "ห้องทำงาน"
+        },
+        {
+            ID: 2,
+            TypeName: "ห้องประชุม"
+        }
+    ]
+
+    const floor = [
+        {
+            ID: 1,
+            Name: "ชั้น 1"
+        },
+        {
+            ID: 2,
+            Name: "ชั้น 2"
+        },
+        {
+            ID: 3,
+            Name: "ชั้น 3"
+        }
+    ]
 
     const items = [
         {
@@ -34,39 +114,56 @@ function CreateMaintenanceRequest() {
                     </div>
                     <p className="text-title">รายการแจ้งซ่อม</p>
                 </div>
+                <div className="hexagon-bg"></div>
                 <form className="form-container">
                     <div className="column">
                         <label className="label-title-text">บริเวณที่ต้องการแจ้งซ่อม</label>
                         <div className="radio-input-box">
-                            <label className="custom-radio">
-                                <input
-                                    type="radio"
-                                    name="position"
-                                    value={1}
-                                    checked={selectedValue === "1"}
-                                    onChange={(e) => setSelectedValue(e.target.value)}
-                                />
-                                <span className="checkmark"></span>{"ห้องทำงาน/ห้องประชุม"}
-                            </label>
-                            <label className="custom-radio">
-                                <input
-                                    type="radio"
-                                    name="position"
-                                    value={2}
-                                    checked={selectedValue === "2"}
-                                    onChange={(e) => setSelectedValue(e.target.value)}
-                                />
-                                <span className="checkmark"></span>{"ภายในและบริเวณรอบอาคาร"}
-                            </label>
+                            {
+                                area.map((item, index) => {
+                                    return (
+                                        <label key={index} className="custom-radio">
+                                            <input
+                                                type="radio"
+                                                name="position"
+                                                value={item.ID}
+                                                checked={selectedArea === item.ID}
+                                                onChange={(e) => setSelectedArea(Number(e.target.value))}
+                                            />
+                                            <span className="checkmark"></span>{item.AreaName}
+                                        </label>
+                                    )
+                                })
+                            }
                         </div>
-                        <label className="label-title-text">ประเภทห้อง</label>
-                        <select
-                            name=""
-                            id=""
-                            className="custom-select"
-                        >
-                            <option value="1">test</option>
-                        </select>
+                        {
+                            selectedArea === 1 ? (
+                                <>
+                                    <label className="label-title-text">ประเภทห้อง</label>
+                                    <select
+                                        name=""
+                                        id=""
+                                        className="custom-select"
+                                        onChange={(e)=>setSelectedRoomType(Number(e.target.value))}
+                                    >
+                                        {
+                                            roomtype.map((item, index) => {
+                                                return (
+                                                    <option key={index} value={item.ID}>{item.TypeName}</option>
+                                                )
+                                            })
+                                        }
+                                    </select>
+                                </>
+                            ) : (
+                                <textarea
+                                    className="description-area"
+                                    placeholder="ระบุรายละเอียดของบริเวณ หรือตำแหน่งที่ต้องการแจ้งซ่อม"
+                                    onChange={(e) => setAreaDescription(e.target.value)}
+                                ></textarea>
+                            )
+                        }
+
                         <div className="selection-box">
                             <div className="subbox">
                                 <label className="label-title-text">ตำแหน่ง/ชั้น</label>
@@ -74,49 +171,103 @@ function CreateMaintenanceRequest() {
                                     name=""
                                     id=""
                                     className="custom-select"
+                                    onChange={(e)=>setSelectedFloor(Number(e.target.value))}
                                 >
-                                    <option value="1">{"ชั้น 1"}</option>
+                                    {
+                                        floor.map((item, index) => {
+                                            return (
+                                                <option key={index} value={item.ID}>{item.Name}</option>
+                                            )
+                                        })
+                                    }
                                 </select>
                             </div>
-                            <div className="subbox">
-                                <label className="label-title-text">หมายเลขห้อง</label>
-                                <select
-                                    name=""
-                                    id=""
-                                    className="custom-select"
-                                >
-                                    <option value="1">{"A01"}</option>
-                                </select>
-                            </div>
+                            {
+                                selectedArea === 1 ? (
+                                    <div className="subbox">
+                                        <label className="label-title-text">หมายเลขห้อง</label>
+                                        <select
+                                            name=""
+                                            id=""
+                                            className="custom-select"
+                                            onChange={(e)=>setSelectedRoom(Number(e.target.value))}
+                                        >
+                                            <option value="1">{"A01"}</option>
+                                        </select>
+                                    </div>
+                                ) : (
+                                    <></>
+                                )
+                            }
                         </div>
                         <label className="label-title-text">รายละเอียดงาน</label>
-                        <textarea name="" id=""></textarea>
+                        <textarea 
+                            onChange={(e)=>setTaskDescription(e.target.value)}
+                            placeholder="ระบุรายละเอียดของงาน หรือปัญหาที่ต้องการแจ้งซ่อม"
+                        ></textarea>
                     </div>
                     <div className="column">
                         <label className="label-title-text">ผู้เขียนคำร้อง</label>
-                        <input type="text" />
+                        <input type="text" value={name} readOnly disabled />
                         <label className="label-title-text">ข้อมูลการติดต่อ</label>
                         <div className="input-contact-box">
                             <div className="sub-box">
                                 <div className="icon-box">
                                     <img src="icons/phone.png" alt="" />
                                 </div>
-                                <input type="text" />
+                                <input
+                                    type="text"
+                                    defaultValue={phone}
+                                    onChange={(e) => setPhone(e.target.value)}
+                                    disabled={!onEdit}
+                                />
                             </div>
                             <div className="sub-box">
                                 <div className="icon-box">
                                     <img src="icons/email.png" alt="" />
                                 </div>
-                                <input type="text" />
+                                <input
+                                    type="text"
+                                    defaultValue={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    disabled={!onEdit}
+                                />
                             </div>
-                            <div className="edit-button">
-                                <img src="icons/edit.png" alt="" className="icon" />
-                                <span>แก้ไข</span>
-                            </div>
+                            {
+                                !onEdit ? (
+                                    <div className="button-container">
+                                        <div
+                                            className="edit-button button-secondary"
+                                            onClick={() => setOnEdit(!onEdit)}
+                                        >
+                                            <img src="icons/edit.png" alt="" className="icon" />
+                                            <span>แก้ไข</span>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <div className="button-container">
+                                        <div
+                                            className="cancel-button button-secondary"
+                                            onClick={() => setOnEdit(!onEdit)}
+                                        >
+                                            <img src="icons/cancel.png" alt="" className="icon" />
+                                            <span>ยกเลิก</span>
+                                        </div>
+                                        <div
+                                            className="save-button button-secondary"
+                                        // onClick={}
+                                        >
+                                            <img src="icons/save.png" alt="" className="icon" />
+                                            <span>บันทึก</span>
+                                        </div>
+                                    </div>
+                                )
+                            }
+
                         </div>
                         <label className="label-title-text">ช่วงเวลาที่รับบริการได้</label>
                         <div className="input-checkbox-box">
-                            <input type="checkbox" className="checkbox"/>
+                            <input type="checkbox" className="checkbox" />
                             <span>ทุกช่วงเวลา</span>
                         </div>
                         <div className="input-time-box">
@@ -125,10 +276,45 @@ function CreateMaintenanceRequest() {
                             <input type="time" />
                         </div>
                     </div>
-                    <div className="column">
-                        <label className="label-title-text">ภาพประกอบ</label>
-                        <input type="file" />
-                        <button type="submit">ส่งคำร้อง</button>
+                    <div className="column" id="column-3">
+                        <div className="input-image-box">
+                            <label className="label-title-text">ภาพประกอบ</label>
+                            {
+                                files.length != 0 ? (
+                                    <div className="show-image-container">
+                                        {files.map((file, index) => (
+                                            <img
+                                                key={index}
+                                                src={URL.createObjectURL(file)}
+                                                alt={`Uploaded ${index + 1}`}
+                                                id={`image-${index + 1}`}
+                                                className="image"
+                                            />
+                                        ))}
+                                    </div>
+                                ) : (
+                                    <></>
+                                )
+                            }
+
+                            <div
+                                onDragOver={(e) => e.preventDefault()}
+                                onDrop={handleDrop}
+                                onClick={() => fileInputRef.current?.click()}
+                                className="drag-drop-image-box"
+                            >
+                                <p>ลากไฟล์มาวางที่นี่ หรือคลิกเพื่อเลือกไฟล์ (สูงสุด 3 ไฟล์)</p>
+                                <input
+                                    ref={fileInputRef}
+                                    type="file"
+                                    accept="image/*"
+                                    multiple
+                                    onChange={handleFileChange}
+                                    style={{ display: "none" }}
+                                />
+                            </div>
+                        </div>
+                        <button type="submit" className="submit-button">ส่งคำร้อง</button>
                     </div>
                 </form>
             </div>
