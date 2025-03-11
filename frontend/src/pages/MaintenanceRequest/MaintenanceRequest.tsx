7c6d8443a8fcec6196ef3f6c9688a2fc9339d82c
poonchub <141898614+poonchub@users.noreply.github.com>
2025-03-12 01:02:06 +0700

    update MaintenanceRequest page

--- frontend/src/pages/MaintenanceRequest/MaintenanceRequest.tsx
+++ frontend/src/pages/MaintenanceRequest/MaintenanceRequest.tsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom"
 import "./MaintenanceRequest.css"
+import { Card, CardContent, Container, FormControl, Grid2, InputLabel, MenuItem, Select, Stack, TextField, Typography } from "@mui/material"
+import { CheckCircle } from "@mui/icons-material"
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
 
 function MaintenanceRequest() {
 
@@ -11,33 +15,78 @@ function MaintenanceRequest() {
     ]
 
     return (
-        <div className="maintenance-request-page">
-            <div className="card">
-                <div className="header-title">
-                    <div className="circle-icon">
-                        <img src="icons/maintenance.png" alt="" />
-                    </div>
-                    <p className="text-title">รายการแจ้งซ่อม</p>
-                </div>
-                <div className="data-query-section">
-                    <div className="status-button-container">
-                        <div className="status-button btn-active">ทั้งหมด</div>
+        <div className="outsider-maintenance-request">
+            <Grid2 container spacing={3}>
+                <Grid2 className='title-box' size={{ xs: 10, md: 12 }}>
+                    <Typography variant="h6" className="title">
+                        รายการแจ้งซ่อม
+                    </Typography>
+                </Grid2>
+                <Grid2 container size={{ xs: 10, md: 8 }}>
+                    {/* Status Section */}
+                    <Grid2 container size={{ xs: 10, md: 12 }} spacing={2} className='status-section'>
                         {
                             status.map((item, index) => {
                                 return (
-                                    <button key={index} className="status-button">
-                                        {item.statusName}
-                                    </button>
+                                    <Grid2 size={{ xs: 10, md: 3 }} key={index}>
+                                        <Card variant="outlined" className="status-card">
+                                            <CardContent>
+                                                <Typography variant="h6" color="textPrimary">{item.statusName}</Typography>
+                                                <div className="status-item success">
+                                                    <CheckCircle /> <span>3 รายการ</span>
+                                                </div>
+                                            </CardContent>
+                                        </Card>
+                                    </Grid2>
                                 )
                             })
                         }
-                    </div>
-                    <Link to="/create-maintenance-request" className="create-request-button">
-                        <img src="icons/writing.png" alt="" />
-                        <span>เขียนคำร้องแจ้งซ่อม</span>
-                    </Link>
-                </div>
-            </div>
+                    </Grid2>
+                    {/* Filters Section */}
+                    <Grid2 container size={{ xs: 10, md: 12 }} spacing={2} className='filter-section'>
+                        <Grid2 size={{ xs: 10, md: 6 }}>
+                            <TextField
+                                fullWidth
+                                className="search-box"
+                                variant="outlined"
+                                placeholder="ค้นหา"
+                                margin="none"
+                            />
+                        </Grid2>
+                        <Grid2 size={{ xs: 10, md: 3 }}>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker
+                                // renderInput={(params) => <TextField {...params} />}
+                                />
+                            </LocalizationProvider>
+                        </Grid2>
+                        <Grid2 size={{ xs: 10, md: 3 }}>
+                            <FormControl fullWidth>
+                                {/* <InputLabel id="demo-simple-select-label">Age</InputLabel> */}
+                                <Select
+                                    // labelId="demo-simple-select-label"
+                                    // id="demo-simple-select"
+                                    // value={age}
+                                    // label="Age"
+                                    // onChange={handleChange}
+                                    defaultValue={1}
+                                >
+                                    {
+                                        status.map((item, index) => {
+                                            return (
+                                                <MenuItem key={index} value={index+1}>{item.statusName}</MenuItem>
+                                            )
+                                        })
+                                    }
+                                </Select>
+                            </FormControl>
+                        </Grid2>
+                    </Grid2>
+                </Grid2>
+                <Grid2 size={{ xs: 10, md: 4 }} sx={{ bgcolor: "#212121" }}>
+
+                </Grid2>
+            </Grid2>
         </div>
     )
 }
