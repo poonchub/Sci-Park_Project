8bc350cf33c6bfec5d8c79d09f54b77345dac888
Warawut <toyzabanana@gmail.com>
2025-06-30 14:58:15 +0700

    edit type entity

--- backend/entity/TimeSlot.go
+++ backend/entity/TimeSlot.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type TimeSlot struct {
     gorm.Model
 	TimeSlotName string `valid:"required~กรุณาระบุช่วงเวลา"`
-	StartTime    string `valid:"required~กรุณาระบุเวลาเริ่มต้น"`
-	EndTime      string	`valid:"required~กรุณาระบุเวลาสิ้นสุด"` 
+	StartTime    time.Time `valid:"required~กรุณาระบุเวลาเริ่มต้น"`
+	EndTime      time.Time	`valid:"required~กรุณาระบุเวลาสิ้นสุด"` 
 	BookingRoom []BookingRoom `gorm:"foreignKey:TimeSlotID"`
 	Roomprice    []Roomprice `gorm:"foreignKey:TimeSlotID"`
 }	
